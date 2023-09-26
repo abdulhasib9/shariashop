313486b18b22fb0b3c7d10186f31cf0893881fea
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-09-25 23:13:09 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './sinppets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
